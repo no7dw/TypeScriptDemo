2bafa231b0f51b82242046ee4bc1fa433d20aba1
no7david <wdeng@kalengo.com>
2016-06-12 09:23:04 +0800

    first http test

---
+++ TcpServer.ts
@@ -0,0 +1,8 @@
+import net = require('net');
+
+var server = net.createServer(function (socket) {
+    socket.write('Echo server\r\n');
+    socket.pipe(socket);
+});
+
+server.listen(1337, '127.0.0.1');
